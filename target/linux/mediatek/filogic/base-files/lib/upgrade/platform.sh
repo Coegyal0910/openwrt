226cfb98be6e8763def7565e613816c2a19c7e1b
Alen <100553064+Coegyal0910@users.noreply.github.com>
2026-09-24 20:21:27 +0800

    Update platform.sh
    Signed-off-by: Alen <100553064+Coegyal0910@users.noreply.github.com>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -134,7 +134,6 @@ platform_do_upgrade() {
 	netis,nx32u|\
 	nokia,ea0326gmp|\
 	openwrt,one|\
-	netcore,n60|\
 	netcore,n60-pro|\
 	qihoo,360t7|\
 	qihoo,360t7-ubi|\
@@ -337,7 +336,6 @@ platform_check_image() {
 	netis,eap930-v1|\
 	netis,nx32u|\
 	openwrt,one|\
-	netcore,n60|\
 	qihoo,360t7|\
 	qihoo,360t7-ubi|\
 	routerich,ax3000-ubootmod|\
